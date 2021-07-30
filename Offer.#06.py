d6fb3614d9dd9c12df93580b8867d1f1483cb061
Neal <neal0408@outlook.com>
2021-07-30 17:30:49 +0800

    Offer 06.从尾到头打印链表

--- Offer.#06.py
+++ Offer.#06.py
@@ -1,5 +1,10 @@
-head = [0, 1, 2]
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-a, b, c = 1, 2, 3
 
-c = [a, b][a > b]
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        return self.reversePrint(head.next) + [head.val] if head else []

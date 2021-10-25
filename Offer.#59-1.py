fdbde36e2fa2ae2c40da73a4392adb2c69e527b7
Neal <neal0408@outlook.com>
2021-10-25 11:38:36 +0800

    #59-1(239).滑动窗口的最大值

--- Offer.#59-1.py
+++ Offer.#59-1.py
@@ -1,20 +1,59 @@
 # 59-1(239).滑动窗口的最大值
 # 给定一个数组nums和滑动窗口的大小k，请找出所有滑动窗口里的最大值
 # 解题思路
-# 1.切片窗口大小，取最大值写入需要返回的列表中
-# 2.
+# 1.切片窗口大小,取最大值写入需要返回的列表中,在题目中没看到对时间复杂度的要求,翻了下题解说是时间复杂度超了.
+# 2.优先队列,使用堆队列的大根堆特性维护最大值,以及将即将离开滑动窗口的元素删除.
+# 3.单调队列,需要同时弹出队尾和队首元素所以需要使用双端队列.
+import collections
 from typing import List
+import heapq
 
 
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    def maxSlidingWindow1(self, nums: List[int], k: int) -> List[int]:
         result = []
         if len(nums) > 0:
             for i in range(len(nums) - k + 1):
                 result.append(max(nums[i: i + k]))
         return result
 
+    def maxSlidingWindow2(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        q = [(-nums[i], i) for i in range(k)]
+        heapq.heapify(q)
+
+        ans = [-q[0][0]]
+        for i in range(k, n):
+            heapq.heappush(q, (-nums[i], i))
+            # 小于滑动窗口左侧的数据弹出
+            while q[0][1] <= i - k:
+                heapq.heappop(q)
+            ans.append(-q[0][0])
+        return ans
+
+    def maxSlidingWindow3(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        q = collections.deque()
+        # 第一个窗口部分
+        for i in range(k):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+
+        ans = [nums[q[0]]]
+        for i in range(k, n):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            while q[0] <= i - k:
+                q.popleft()
+            ans.append(nums[q[0]])
+        
+        return ans
+
 
 s = Solution()
 num1 = [1, 3, -1, -3, 5, 3, 6, 7]
-result1 = s.maxSlidingWindow(num1, 3)
+# res = s.maxSlidingWindow1(num1, 3)
+# res = s.maxSlidingWindow2(num1, 3)
+res = s.maxSlidingWindow3(num1, 3)

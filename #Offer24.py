15866293618ca64ba18c3b949af97bfbd9501011
Neal <neal0408@outlook.com>
2021-10-29 16:00:47 +0800

    #Offer24.反转链表

--- #Offer24.py
+++ #Offer24.py
@@ -1,51 +1,50 @@
-# Offer24(206).反转链表
-# 给你单链表的头结点 head,请你反转链表,并返回反转后的链表.
+# Offer24.反转链表
+# 定义一个函数,输入一个链表的头结点,反转该链表并输出反转后链表的头节点.
 # 解题思路
-# 1.最开始干想有点没想到,后来看了流程图才一点点推的,迭代做法就是搞个 pre 存前节点, cur 存当
-# 前节点,tmp 存后继节点,一步步循环改指向.难道是不难画画图就好了.
-# 2.递归就比较巧妙,层层套用循环到 head 为尾时终止并返回,然后重新改指向.这个解释有点不太直观
-# ,看一下那个流程图会舒服很多.
+# 自己写的时候还是很笨重,先在数组中存储数值,再反转再重头开始重新创建新的链表,但代码不够优雅.
+# 在别人的题解中迭代是只使用的一个循环来完成的,还有利用Python平行语言赋值语法特性进一步简化.
+# 另外一种思路就是递归,很巧妙但是想一次写对比较有难度.
 
 
 class ListNode:
-
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
 
     def reverseList1(self, head: ListNode) -> ListNode:
-        pre = None
-        cur = head
+        cur, pre = head, None
         while cur:
             tmp = cur.next
             cur.next = pre
             pre = cur
             cur = tmp
-
+        return pre
+    
+    def reverseList2(self, head: ListNode) -> ListNode:
+        cur, pre = head, None
+        while cur:
+            cur.next, pre, cur = pre, cur, cur.next
         return pre
 
-    def reverseList2(self, head: ListNode) -> ListNode:
-        if head == None or head.next == None:
-            return head
-        cur = self.reverseList2(head.next)
-        head.next.next = head
-        head.next = None
+    def reverseList3(self, head: ListNode) -> ListNode:
+        def recur(cur, pre):
+            if not cur: return pre
+            res = recur(cur.next, cur)
+            cur.next = pre
+            return res
 
-        return cur
+        return recur(head, None)
 
 
-s = Solution()
-# 创建链表
-var = [1, 2, 3, 4, 5]
-dummyRoot = ListNode(0)
-ptr = dummyRoot
-for i in var:
-    ptr.next = ListNode(i)
-    ptr = ptr.next
-head1 = dummyRoot.next
-# 使用方法获取结果
-# res = s.reverseList2(head1)
-res = s.reverseList1(head1)
+p = q = ListNode(0)
+for i in range(1, 6):
+    p.next = ListNode(i)
+    p = p.next
+q = q.next
+obj = Solution()
+# result = obj.reverseList1(q)
+# result = obj.reverseList2(q)
+result = obj.reverseList3(q)

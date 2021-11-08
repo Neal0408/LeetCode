06fa1e1615875636769c5613d2e4292e64d55d98
Neal <neal0408@outlook.com>
2021-11-08 12:12:04 +0800

    #101.对称二叉树

---
+++ #101.py
@@ -0,0 +1,57 @@
+# 101.对称二叉树
+# 给定一个二叉树,检查它是否是镜像对称的.
+# 解题思路
+# 想想怎么表达左子树和右子树.递归的终止条件有三个,一左右节点都为空,二俩节点中其中一个为空,三
+# 俩个节点的值不相等.再递归比较其左节点的左孩子和右节点的右孩子以及左节点的右孩子和右节点的左
+# 孩子是否相同.
+# 还有一种做法就是用队列去做,把根的左右节点插入到队列中,比较这两个节点.
+
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+
+        def dfs(left, right):
+            if not (left or right):
+                return True
+            if not (left and right):
+                return False
+            if left.val != right.val:
+                return False
+            return dfs(left.left, right.right) and dfs(left.right, right.left)
+
+        return dfs(root.left, root.right)
+
+    def isSymmetric2(self, root: TreeNode) -> bool:
+        if not root or not (root.left or root.right):
+            return True
+        queue = [root.left, root.right]
+        while queue:
+            left = queue.pop(0)
+            right = queue.pop(0)
+
+            if not (left or right):
+                continue
+            if not (left and right):
+                return False
+            if left.val != right.val:
+                return False
+            queue.append(left.left)
+            queue.append(right.right)
+
+            queue.append(left.right)
+            queue.append(right.left)
+        return True
+
+
+obj = Solution()

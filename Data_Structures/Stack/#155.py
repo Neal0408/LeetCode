940c7b8c538ecd09dc6dfd0a515dee8a4d2b21d5
Neal <neal0408@outlook.com>
2022-02-19 19:32:27 +0800

    #155.最小栈

---
+++ Data_Structures/Stack/#155.py
@@ -0,0 +1,29 @@
+# 155.最小栈
+# 设计一个支持 push，pop，top操作，并能在常熟时间内检索到最小元素的栈。
+# 解题思路
+# 1.比较经典的设计题，用一个辅助栈来保存现在的最小值。但是写的还是没有题解简约。还是值得学习。
+import math
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = [math.inf]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.min.append(min(val, self.stack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
+
+
+obj = MinStack()

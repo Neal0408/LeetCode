1dcad3c46b29042079f3cbca43e3b2dc8981c2b7
Neal <neal0408@outlook.com>
2022-05-05 13:46:29 +0800

    #104.二叉树的最大深度 #二叉树经典题目

--- Data_Structures/Tree/#104.py
+++ Data_Structures/Tree/#104.py
@@ -2,6 +2,10 @@
 # 给定一个二叉树，找出其最大深度。
 # 解题思路
 # 1.自底向上，当前高度的定义是取左右子树高度的最大值加一。终止条件是在没有元素遍历了即为终止。
+# 相当于后序遍历。
+# 2.自顶向下，相当于前序遍历。因为要额外维护一个类变量。
+# 3.BFS广度优先遍历，也是层序遍历。一层一层遍历到最后高度就是深度。
+
 from typing import Optional
 
 
@@ -21,6 +25,39 @@ class Solution:
         rightHeight = self.maxDepth(root.right)
 
         return max(leftHeight, rightHeight) + 1
+
+    def dfs(self, root, cur):
+        if not root:
+            return 0
+        cur += 1
+        if not root.left and not root.right:
+            self.depth = max(self.depth, cur)
+        self.dfs(root.left, cur)
+        self.dfs(root.right, cur)
+        return self.depth
+
+    def maxDepth2(self, root: Optional[TreeNode]) -> int:
+        cur = 0
+        self.depth = 0
+        return self.dfs(root, cur)
+
+    def maxDepth3(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = [root]
+        depth = 0
+
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            depth += 1
+
+        return depth
 
 
 obj = Solution()

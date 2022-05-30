d1f4a80db8d1e3cbed7de2b99a068c5269d98245
Neal <neal0408@outlook.com>
2022-05-30 13:17:40 +0800

    #Offer 05.替换空格 #字符串

--- 剑指 Offer/#Offer_05.py
+++ 剑指 Offer/#Offer_05.py
@@ -1,17 +1,19 @@
 # 剑指 Offer 05.替换空格
-# 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+# 请实现一个函数，把字符串s中的每个空格替换成"%20"。
 # 解题思路
-# 1.因为 Python 中字符串为不可变元素，所以要么用列表，要么用字符串的 replace 方法来替换。
+# 1.因为Python中字符串为不可变元素，所以要么用列表，要么用字符串的 replace 方法来替换。
 
 
 class Solution:
 
     def replaceSpace(self, s: str) -> str:
-        a = list(s)
-        for i in range(len(a)):
-            if a[i] == " ":
-                a[i] = "%20"
-        return ''.join(a)
+        res = []
+        for i in s:
+            if i == ' ':
+                res.append('%20')
+            else:
+                res.append(i)
+        return ''.join(res)
 
 
 obj = Solution()
